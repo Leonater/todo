397f6f04bf3f6727bfa4002d8cf861ac73db9bd9
Kevjas1301 <148619253+Kevjas1301@users.noreply.github.com>
2024-12-21 08:54:30 +0000

    Cypress test

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -1,5 +1,9 @@
 const PORT = process.env.PORT || 3000;
 
+const today = new Date();
+const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+const containsDate = `${String(today.getMonth() + 1).padStart(2, '0')}/${String(today.getDate()).padStart(2, '0')}/${today.getFullYear()}`;
+
 describe('Todo', function () {
 
     beforeEach(() => {
@@ -12,46 +16,54 @@ describe('Todo', function () {
 
     it('Fügt ein neues Todo hinzu und zeigt es an', function () {
         cy.get('#todo').type('Neue Aufgabe');
-        cy.get('#due').type('2024-11-20');
+        cy.get('#due').type(formattedDate);
         cy.get('#status').select('offen');
         cy.get('[type="submit"]').click();
     
-        cy.get('#todo-list .todo').should('have.length', 1);
+        cy.get('#todo-list .todo').should('have.length.at.least', 1);
         cy.contains('Neue Aufgabe');
-        cy.contains('11/20/2024');
+        cy.contains(containsDate);
         cy.contains('offen');
     });
 
     it('Zeigt eine Fehlermeldung, wenn kein Titel eingegeben wurde', function () {
-        cy.get('#due').type('2024-11-20');
+        cy.get('#due').type(formattedDate);
         cy.get('[type="submit"]').click();
         
         cy.get('#todo').invoke('prop', 'validationMessage').should('not.be.empty');
     });
 
-    it('Ändert den Status eines Todos', function () {
-        cy.get('#todo').type('Another Task');
-        cy.get('#due').type('2024-11-21');
+    it('Zeigt eine Fehlermeldung, wenn ein Fälligkeitsdatum in der Vergangenheit liegt', function () {
+        cy.get('#todo').type('Falsches Datum');
+        cy.get('#due').type('2020-11-20');
         cy.get('[type="submit"]').click();
         
-        cy.contains('.todo', 'Another Task')
+        cy.get('#due').invoke('prop', 'validationMessage').should('not.be.empty');
+    });
+
+    it('Ändert den Status eines Todos', function () {
+        cy.get('#todo').type('Status ändern');
+        cy.get('#due').type(formattedDate);
+        cy.get('[type="submit"]').click();
+        
+        cy.contains('.todo', 'Status ändern')
             .find('.status')
             .click();
 
-        cy.contains('.todo', 'Another Task') 
+        cy.contains('.todo', 'Status ändern') 
             .find('.status')
             .should('contain', 'in Bearbeitung');
     });
 
     it('Löscht ein Todo', function () {
-        cy.get('#todo').type('Task to Delete');
-        cy.get('#due').type('2024-11-22');
+        cy.get('#todo').type('Aufgabe löschen');
+        cy.get('#due').type(formattedDate);
         cy.get('[type="submit"]').click();
 
-        cy.contains('.todo', 'Task to Delete')
+        cy.contains('.todo', 'Aufgabe löschen')
             .find('.delete')
             .click();
         
-        cy.contains('.todo', 'Task to Delete').should('not.exist');
+        cy.contains('.todo', 'Aufgabe löschen').should('not.exist');
     });
 });
